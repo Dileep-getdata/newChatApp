396d7782d4cd388a7f48c863644c2a4b3b72c732
Dileep Thiriveedhi <dileep2424@gmail.com>
2022-11-26 11:44:30 +0530

    Lets make the Chat App real time by setInterval for get API

--- public/chatApp/chat.js
+++ public/chatApp/chat.js
@@ -2,33 +2,36 @@ const display=document.getElementById('displayMsg');
 const token=localStorage.getItem('token');
 
 window.addEventListener('DOMContentLoaded', async ()=>{
+    gettingMsg();
+   
+})
+
+async function gettingMsg(){
     try{
         const displayMsg= document.getElementById('displayMsg');
         let innerList=`<ul>`
         const response=await axios.get(`http://localhost:3050/chat/chat`,{headers:{'Authentication':token}});
         const usersN=response.data.users;
         const chatMsgs=response.data.chat;
-        usersN.forEach(user=>{
-           
+        usersN.forEach(user=>{           
             const firstNme=(user.userName).split(' ')[0];
             innerList += `<li>${firstNme} Join !</li>`;
             
-        })
-       
+        })       
         chatMsgs.forEach(chatMsg=>{
             innerList += `<li>${chatMsg.message}</li>`;
             
         })
         displayMsg.innerHTML=innerList+`</ul>`;
-
-        
     }catch(err){
         console.log(err);
     }
-   
-})
+}
+//
+setInterval(()=>gettingMsg(),1000)
 
-
+// 
+// 
 const chatMsg=document.getElementById('chatMsg');
 chatMsg.addEventListener('submit',async(e)=>{
     try{
