3256ab99e14b995586dd3e208125c90f260176be
Dileep Thiriveedhi <dileep2424@gmail.com>
2022-11-29 11:20:35 +0530

    mistake correction

--- public/chatApp/createGroup.js
+++ public/chatApp/createGroup.js
@@ -6,7 +6,7 @@ groupFrom.addEventListener('submit',async (e)=>{
     try{
         e.preventDefault();
         const name=e.target.groupName.value;
-        const response=await axios.post('http://http://3.84.17.108:3050/group/creategroup',{name:name},{headers:{'Authentication':token}});
+        const response=await axios.post('http://3.84.17.108:3050/group/creategroup',{name:name},{headers:{'Authentication':token}});
         console.log(response);
     }catch(err){
         console.log(err);
